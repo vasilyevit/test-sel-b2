73b89b14c72306c2416c9e33ac2e2503e78ad116
Vasiliev Alexandr <itvaa@ya.ru>
2019-02-08 17:10:58 +0300

    просто какой-то тест
    тест, который просто запускает браузер, открывает какую-нибудь страницу и закрывает браузер

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("https://duckduckgo.com/");
+        driver.findElement(By.name("q")).sendKeys("sel-b2");
+        driver.findElement(By.id("search_button_homepage")).click();
+        wait.until(titleIs("sel-b2 at DuckDuckGo"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
