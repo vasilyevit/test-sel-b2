d2e4efdca63be4d1ddfc21d03218c0a9483f4037
Vasiliev Alexandr <itvaa@ya.ru>
2019-02-22 13:40:39 +0300

    Update task 13

--- src/test/java/MyTestSupport.java
+++ src/test/java/MyTestSupport.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class MyTestSupport extends MyTestBase{
@@ -18,7 +21,13 @@ public class MyTestSupport extends MyTestBase{
      * @param xpath - XPath элемента, изчезновение которого мы будем ждать
      */
     public void waitUntilElementPresent(String xpath){
-        WebDriverWait wait = new WebDriverWait(driver, 500);
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(xpath)));
+        try {
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            WebDriverWait wait = new WebDriverWait(driver, 500);
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(xpath)));
+        } finally {
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+
     }
 }
