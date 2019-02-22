496d5969e8949eaf21e3d1526adc2336333ec84c
Vasiliev Alexandr <itvaa@ya.ru>
2019-02-22 13:20:10 +0300

    Задание 13

--- src/test/java/MyTestBase.java
+++ src/test/java/MyTestBase.java
@@ -1,12 +1,10 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyTestBase {
     public WebDriver driver;
@@ -15,16 +13,9 @@ public class MyTestBase {
     @Before
     public void start() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 5);
     }
-
-    /*@Test
-    public void myFirstTest(){
-        driver.get("https://duckduckgo.com/");
-        driver.findElement(By.name("q")).sendKeys("sel-b2");
-        driver.findElement(By.id("search_button_homepage")).click();
-        wait.until(titleIs("sel-b2 at DuckDuckGo"));
-    }*/
 
     @After
     public void stop() {
--- src/test/java/MyTestSupport.java
+++ src/test/java/MyTestSupport.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
@@ -10,5 +11,14 @@ public class MyTestSupport extends MyTestBase{
     public void waitXpath(String xpath) {
         WebDriverWait wait = new WebDriverWait(driver, 500);
         wait.until(elementToBeClickable(By.xpath(xpath)));
+    }
+
+    /**
+     * Ждем пока указанный элемент не пропадет из видимости
+     * @param xpath - XPath элемента, изчезновение которого мы будем ждать
+     */
+    public void waitUntilElementPresent(String xpath){
+        WebDriverWait wait = new WebDriverWait(driver, 500);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(xpath)));
     }
 }
